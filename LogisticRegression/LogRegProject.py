d6db431d500a6abbdeabc0e476186e7fbe1da98f
Kedar <kedarpujara@gmail.com>
2018-02-01 22:09:18 -0800

    git not working trying reset

---
+++ LogisticRegression/LogRegProject.py
@@ -0,0 +1,49 @@
+import pandas as pd 
+import seaborn as sns 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+def LogRegProject():
+
+	ads = pd.read_csv('advertising.csv')
+
+	
+
+	ads.drop(['Ad Topic Line', 'City', 'Country', 'Timestamp'], axis=1, inplace=True)
+	print(ads.head(2))
+
+
+	#sns.distplot(ads['Age'], kde=False, bins=30)
+	sns.jointplot(x=ads['Area Income'], y=ads['Age'])
+
+
+
+	from sklearn.model_selection import train_test_split
+	X = ads.drop('Clicked on Ad', axis=1)
+	y = ads['Clicked on Ad']
+
+	x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.3, 
+		random_state=101)
+
+
+
+	from sklearn.linear_model import LogisticRegression
+	logReg = LogisticRegression()
+
+	logReg.fit(x_train, y_train)
+
+	predictions = logReg.predict(x_test)
+
+
+	from sklearn.metrics import classification_report
+
+	print(classification_report(y_test, predictions))
+
+	#plt.show()
+
+
+
+
+
+LogRegProject()
--- LogisticRegression/logReg.py
+++ LogisticRegression/logReg.py
@@ -10,28 +10,28 @@ def LogisticReg():
 
 	titTrain.head()
 
-	#Line below is used to show which data we are missing
-	#sns.heatmap(titTrain.isnull(), yticklabels=False, cbar=False,cmap='viridis')
-
+	#---Graph the data to get a better understanding of relationships and correlations---#
+	sns.heatmap(titTrain.isnull(), yticklabels=False, cbar=False,cmap='viridis')
 	
+
 	sns.set_style('whitegrid')
-	#sns.countplot(x='Survived', hue='Sex', data=titTrain) # Categorize by sex
-	#sns.countplot(x='Survived', hue='Pclass', data=titTrain) # Categorize by passenger class
-	#sns.distplot(titTrain['Age'].dropna(),kde=False,bins=30)
+	sns.countplot(x='Survived', hue='Sex', data=titTrain) # Categorize by sex
+	sns.countplot(x='Survived', hue='Pclass', data=titTrain) # Categorize by passenger class
+	sns.distplot(titTrain['Age'].dropna(),kde=False,bins=30)
 
 	titTrain['Age'].hist(bins=30,color='darkred',alpha=0.7)
 	titTrain['Fare'].hist(bins=40,figsize=(8,4))
 
 	
-
 	cf.go_offline()
-	#titTrain['Fare'].iplot(kind='hist',bins=30)
-
-
-	# Cleaning the data
+	titTrain['Fare'].iplot(kind='hist',bins=30)
 
 	plt.figure(figsize=(12,7))
-	#sns.boxplot(x='Pclass', y='Age', data=titTrain)
+	sns.boxplot(x='Pclass', y='Age', data=titTrain)
+
+
+
+	#---Get rid/adjust the values we are not interested in---#
 
 	titTrain['Age'] = titTrain[['Age','Pclass']].apply(impute_age,axis=1)
 
@@ -40,10 +40,46 @@ def LogisticReg():
 
 	titTrain.dropna(inplace=True)
 
-	sns.heatmap(titTrain.isnull(), yticklabels=False, cbar=False)
+	sexDummy = pd.get_dummies(titTrain['Sex'], drop_first=True)
+	embarkDummy = pd.get_dummies(titTrain['Embarked'], drop_first=True)
+
+	titTrain.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)
+	titTrain = pd.concat([titTrain, sexDummy, embarkDummy], axis=1)
 
 
-	plt.show()
+
+	#---Train the model---#
+	from sklearn.model_selection import train_test_split
+
+	X = titTrain.drop('Survived', axis=1)
+	y = titTrain['Survived']
+
+	x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.3, 
+		random_state=101)
+
+
+	from sklearn.linear_model import LogisticRegression
+	
+	logModel = LogisticRegression()
+	logModel.fit(x_train, y_train)
+
+	predictions = logModel.predict(x_test)
+
+	
+
+
+	#---Test and evaluate the model---#
+	from sklearn.metrics import classification_report
+
+	print(classification_report(y_test, predictions))
+
+
+	
+
+
+
+
+	#plt.show()
 
 
 
